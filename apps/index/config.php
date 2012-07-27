54ccd90a831436dbaa7dd67897fec0ea4c26d5ef
welling <welling@ld>
2012-07-27 11:14:58 +0800

    slightly change...

--- apps/index/config.php
+++ apps/index/config.php
@@ -1,11 +1,6 @@
 <?php
 /*
  * 注意不要覆盖框架自动生成的配置，
- * 例如 
-[lib_dir] => F:\Web\easyphp\lib
-[project_dir] => F:\Web\easyphp
-[app_dir] => F:\Web\easyphp\apps\index
-[app_name] => index
  */
 return array(
 	'log_tagFilter'=>array(//标签过滤，不显示以下的标签
