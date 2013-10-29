b955b3219b5697b320cf8ba414793cc9a17e990b
welling <welling@ld>
2013-10-29 19:17:07 +0800

    路由修正

--- lib/demo/globalconfig.php
+++ lib/demo/globalconfig.php
@@ -31,13 +31,6 @@ $config=array(
 	
 	//route engine
 	'_route_enable'=>true,
-	//默认路由,将合并到配置路由的最后一行中
-	'_route_default'=>array(
-		'rule'=>'/(.*?)/(.*?)/{0,1}',
-		'controller'=>1,//如果不为数字，那么指定一个默认值
-		'action'=>2,
-		'args'=>array(),//参数
-	),
 	
 	//four types of NotFound
 	//if empty then ignore,but you can see what happen in log.
